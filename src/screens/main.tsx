a550af76017cb27cce99d41173ae98572d77b5ef
Fitrie <mfitrie78@gmail.com>
2023-04-05 21:35:03 +0800

    init

---
+++ src/screens/main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+    Text,
+    Box,
+    Center,
+    VStack,
+    themeTools,
+    useTheme,
+    useColorMode,
+    useColorModeValue,
+} from 'native-base';
+import ThemeToggle from "../components/theme-toggle";
+
+export default function MainScreen(){
+    const { colorMode } = useColorMode();
+    return (
+        <Center 
+            _dark={{bg: 'blueGray.900'}} 
+            _light={{bg: 'blueGray.50'}} 
+            px={4} 
+            flex={1}
+        >
+            <VStack space={5} alignItems="center">
+                <Box>
+                    <Text>Hello</Text>
+                </Box>
+                <ThemeToggle/>
+            </VStack>
+        </Center>
+    )
+}
